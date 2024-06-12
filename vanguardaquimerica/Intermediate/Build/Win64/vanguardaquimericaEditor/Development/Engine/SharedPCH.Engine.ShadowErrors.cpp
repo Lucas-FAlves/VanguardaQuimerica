a0bb0d20bef2c1ca26b56247533b29636ea01ce4
Lucas-FAlves <Lucas.3451@hotmail.com>
2024-06-12 18:19:33 -0300

    Projeto refeito como 100% Blueprint

--- vanguardaquimerica/Intermediate/Build/Win64/vanguardaquimericaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30154
-#include "C:/Users/Cliente/Documents/Unreal Projects/Vanguarda quimerica/vanguardaquimerica/Intermediate/Build/Win64/vanguardaquimericaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
